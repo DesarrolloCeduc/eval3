075be1e17c9b4641f14b88ecf76399a048de9e45
IEstrada <gakuseivan@gmail.com>
2017-10-18 08:03:32 -0300

    Comentario de lineas funcionales
    Comento las lineas como metodo de enseñanza

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -21,11 +21,11 @@ class Welcome extends CI_Controller {
 	function __construct(){
 		parent::__construct();
 		$this->load->helper('form');
-		$this->load->model('form_model');  
+		//$this->load->model('form_model');  
 	}
-	
+
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('formulario');
 	}
 }
